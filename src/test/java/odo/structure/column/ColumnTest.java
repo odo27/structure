e1c86c849b956d62d871d1f36e77ef3aa33cbd88
odo27 <odo27@naver.com>
2022-11-30 00:24:32 +0900

    style: delete unused lines

--- src/test/java/odo/structure/column/ColumnTest.java
+++ src/test/java/odo/structure/column/ColumnTest.java
@@ -6,7 +6,6 @@ import odo.structure.column.section.Rectangle;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
