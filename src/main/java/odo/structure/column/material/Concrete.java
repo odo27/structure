19069a831bc1948405998d3f883550029b92d07e
odo27 <odo27@naver.com>
2022-12-01 12:05:42 +0900

    feat(Concrete): add betaOne

--- src/main/java/odo/structure/column/material/Concrete.java
+++ src/main/java/odo/structure/column/material/Concrete.java
@@ -4,9 +4,23 @@ public class Concrete {
 
     public static final double ecu = 0.003;
     public final double fck;
+    public final double b1;
 
     public Concrete(double compressiveStrength) {
         fck = compressiveStrength;
+        b1 = calculateBetaOne(fck);
+    }
+
+    private double calculateBetaOne(double fck) {
+        if (fck <= 28) {
+            return 0.85;
+        }
+
+        if (fck <= 56) {
+            return 0.85 - 0.007 * (fck - 28);
+        }
+
+        return 0.65;
     }
 
 }
--- src/test/java/odo/structure/column/material/ConcreteTest.java
+++ src/test/java/odo/structure/column/material/ConcreteTest.java
@@ -1,15 +1,24 @@
 package odo.structure.column.material;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ConcreteTest {
 
     @Test
-    void constructorTest() {
+    void fckTest() {
         Concrete concrete = new Concrete(24);
         assertThat(concrete.fck).isEqualTo(24);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"24,0.85", "30,0.836", "50,0.696"})
+    void betaOneTest(double fck, double expected) {
+        Concrete concrete = new Concrete(fck);
+        assertThat(concrete.b1).isEqualTo(expected);
     }
 
 }
