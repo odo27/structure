2d9a182ea21979cd19f6d8223b2c7007b4a1e7ce
odo27 <odo27@naver.com>
2022-12-11 22:35:01 +0900

    feat: add write csv

--- src/main/java/odo/structure/StructureApplication.java
+++ src/main/java/odo/structure/StructureApplication.java
@@ -4,9 +4,12 @@ import odo.structure.analysis.section.BiAxial;
 import odo.structure.column.Column;
 import odo.structure.column.material.Concrete;
 import odo.structure.column.pmcurve.Point3D;
+import odo.structure.column.pmcurve.PureStrength;
 import odo.structure.column.reinforcement.Rebar;
 import odo.structure.column.section.Rectangle;
+import odo.structure.view.WriteCSV;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class StructureApplication {
@@ -25,14 +28,15 @@ public class StructureApplication {
 		Column column = new Column(section, concrete, rebars);
 
 		BiAxial biAxial = new BiAxial(column);
+		List<List<Point3D>> analysisResult = new ArrayList<>();
+
 		List<Point3D> points3D = biAxial.PMCurve(59.71204);
-		int c = 1;
-		for (Point3D point3D : points3D) {
-			System.out.printf("%d, ", c);
-			System.out.printf("%.2f, ", point3D.x / 1e3);
-			System.out.printf("%.2f, ", point3D.y / 1e6);
-			System.out.printf("%.2f%n", point3D.z / 1e6);
-			c++;
-		}
+
+		double pureCompression = PureStrength.compression(section, concrete, rebars);
+		double pureTension = PureStrength.tension(rebars);
+		points3D.add(new Point3D(pureCompression, 0, 0));
+		points3D.add(new Point3D(pureTension, 0, 0));
+
+		WriteCSV.writeBiAxialAnalysisResult(points3D);
 	}
 }
---
+++ src/main/java/odo/structure/view/WriteCSV.java
@@ -0,0 +1,41 @@
+package odo.structure.view;
+
+import odo.structure.column.pmcurve.Point3D;
+
+import java.io.*;
+import java.util.List;
+
+public class WriteCSV {
+
+    public static void writeBiAxialAnalysisResult(List<Point3D> points3D) {
+        File resultCSV = new File("C:/Users/user/Desktop/result.csv");
+        BufferedWriter bw = null;
+        try {
+            bw = new BufferedWriter(new FileWriter(resultCSV));
+
+            int c = 1;
+            for (Point3D point3D : points3D) {
+                String line = String.format("%d, ", c);
+                line += String.format("%.2f, ", point3D.x / 1e3);
+                line += String.format("%.2f, ", point3D.y / 1e6);
+                line += String.format("%.2f, ", point3D.z / 1e6);
+                bw.write(line);
+                bw.newLine();
+                c++;
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (bw != null) {
+                    bw.flush();
+                    bw.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
