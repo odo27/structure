c733aef31e28b8264bfedb19529ebdaba3a1fdaf
odo27 <odo27@naver.com>
2022-12-13 02:22:37 +0900

    feat: add only theta biaxial analysis

--- src/main/java/odo/structure/StructureApplication.java
+++ src/main/java/odo/structure/StructureApplication.java
@@ -4,7 +4,6 @@ import odo.structure.analysis.section.BiAxial;
 import odo.structure.column.Column;
 import odo.structure.column.material.Concrete;
 import odo.structure.column.pmcurve.Point3D;
-import odo.structure.column.pmcurve.PureStrength;
 import odo.structure.column.pmcurve.Quadrant;
 import odo.structure.column.reinforcement.Rebar;
 import odo.structure.column.section.Rectangle;
@@ -16,19 +15,52 @@ import java.util.List;
 public class StructureApplication {
 
 	public static void main(String[] args) {
-		BiAxialAnalysis();
+		biAxialThetaAnalysis();
 	}
 
-	private static void BiAxialAnalysis() {
+	private static void biAxialThetaAnalysis() {
+		Rectangle section = new Rectangle(550, 550);
+		Concrete concrete = new Concrete(27);
+
+		double fy = 400;
+		double As = 506;
+		double space = 60;
+		List<Rebar> rebars = generateRebars(fy, As, space, 3);
+
+		Column column = new Column(section, concrete, rebars);
+		BiAxial biAxial = new BiAxial(column);
+
+		List<Point3D> points3D = biAxial.reducedPMCurve(210.46);
+
+		WriteCSV.writeBiAxialThetaAnalysisResult(points3D);
+	}
+
+	private static List<Rebar> generateRebars(double fy, double As, double space, int n) {
+		double currentX = space * n;
+		double currentY = space * n;
+
+		List<Integer> directionX = List.of(0, -1, 0, 1);
+		List<Integer> directionY = List.of(-1, 0, 1, 0);
+
+		List<Rebar> rebars = new ArrayList<>();
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 2 * n; j++) {
+				currentX += directionX.get(i) * space;
+				currentY += directionY.get(i) * space;
+				rebars.add(new Rebar(fy, As, currentX, currentY));
+			}
+		}
+
+		return rebars;
+	}
+
+	private static void biAxialAnalysis() {
 		Rectangle section = new Rectangle(400, 400);
 		Concrete concrete = new Concrete(21);
 		List<Rebar> rebars = List.of(
 				new Rebar(400, 506, -150, 150),
 				new Rebar(400, 506, 150, -150)
 		);
-
-		double pureCompression = PureStrength.compression(section, concrete, rebars);
-		double pureTension = PureStrength.tension(rebars);
 
 		Column column = new Column(section, concrete, rebars);
 		BiAxial biAxial = new BiAxial(column);
@@ -39,9 +71,6 @@ public class StructureApplication {
 				Quadrant.validateTheta(theta);
 				System.out.println(theta);
 				List<Point3D> points3D = biAxial.PMCurve(theta);
-
-				points3D.add(new Point3D(pureCompression, 0, 0));
-				points3D.add(new Point3D(pureTension, 0, 0));
 
 				analysisResult.add(points3D);
 			} catch (IllegalArgumentException ignored) {
